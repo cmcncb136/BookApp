69d1907b3983912e51a3c634411b7b4d6d0c2db5
KJW <cmcncb13624@gmail.com>
2024-04-03 21:19:02 +0900

    feat : bind bind 기능 추가 및 테스트

--- app/src/main/java/com/heybuddy/safespace/MainActivity.kt
+++ app/src/main/java/com/heybuddy/safespace/MainActivity.kt
@@ -2,10 +2,14 @@ package com.heybuddy.safespace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.heybuddy.safespace.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        bind.tv.text = bind.tv.text.toString() + " : bind"
     }
 }
